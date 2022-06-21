5700cbfa68930592637f3796618271761606f0b5
Vivek Singh <petmongrels@gmail.com>
2022-06-21 18:10:15 +0530

    latest lts of ubuntu

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "bento/ubuntu-20.04"
   config.vm.network "private_network", ip: "192.168.33.11"
   config.vm.network "forwarded_port", guest: 22, host: 6012
   config.vm.hostname = "avni-ubuntu"
--- database/export-import-tables.sh
+++ database/export-import-tables.sh
@@ -6,4 +6,5 @@ for Table in $Tables
 	do
   		echo "Export/Importing table $Table for $1"
   		psql -h localhost -p 2203 -U $1 -c "COPY (SELECT * FROM $Table) TO STDOUT;" openchs | psql -h localhost -U openchs -c "COPY $Table FROM STDIN;" openchs
+  		psql -h localhost -p 2203 -U $1 -c "SELECT setval('$Table_id_seq', COALESCE((SELECT MAX(id)+1 FROM $Table), 1), false);" openchs
   done
